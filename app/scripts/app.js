70672eb6f8d19b25786825ea50043e65754b8cbe
Nestor Gimenez <ngimenez@spinifexit.com>
2015-12-10 09:35:33 +0800

    test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -53,6 +53,11 @@ angular
                 templateUrl: 'views/settings.html',
                 controller: 'SettingsCtrl'
             })
+            .when('/about', {
+              templateUrl: 'views/about.html',
+              controller: 'AboutCtrl',
+              controllerAs: 'about'
+            })
             .otherwise({
                 redirectTo: '/'
             });
